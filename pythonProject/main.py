87f23387b1c3adeb08f44c8c358e4f63962121ff
csardana <csardana18@gmail.com>
2021-02-13 19:42:22 -0500

    done with all links

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -2,27 +2,29 @@ from os.path import exists
 
 import bs4 as bs
 import urllib.request
+links = ['https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020AP.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020ED.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020EU.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020FA.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020GL.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020GS.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020HH.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020LE.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020LW.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020SB.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020SC.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/SU2020UG.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/SU2020GS.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/SU2020LW.html','https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/SU2020SB.html']
 
-source = urllib.request.urlopen(
-    'https://apps1.sis.yorku.ca/WebObjects/cdm.woa/Contents/WebServerResources/FW2020LE.html').read()
-soup = bs.BeautifulSoup(source, 'lxml')
+for link in links:
+    source = urllib.request.urlopen(
+        link).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
 
-table = soup.table
-firstAndSecondYr=0
-thirdAndFourthYr=0
+    table = soup.table
+    firstAndSecondYr=0
+    thirdAndFourthYr=0
 
-table_rows = table.find_all('tr')
-for tr in table_rows:
-    td = tr.find_all('td')
-    row = [i.text for i in td]
-    if "LECT\xa0" in row:
-       x = row[1].split(" ")
-       if int(x[0]) >= 1000 & int(x[0]) < 3000:
-           firstAndSecondYr = firstAndSecondYr + 80
-       if int(x[0]) >= 3000 & int(x[0]) < 5000:
-           thirdAndFourthYr =  thirdAndFourthYr + 30
+    table_rows = table.find_all('tr')
+    for tr in table_rows:
+        td = tr.find_all('td')
+        row = [i.text for i in td]
+        if "LECT\xa0" in row:
+           x = row[1].split(" ")
+           if int(x[0]) >= 1000 & int(x[0]) < 3000:
+               firstAndSecondYr = firstAndSecondYr + 80
+           if int(x[0]) >= 3000 & int(x[0]) < 5000:
+               thirdAndFourthYr =  thirdAndFourthYr + 30
 
-totalStudents = firstAndSecondYr + thirdAndFourthYr
-print(firstAndSecondYr)
-print(thirdAndFourthYr)
-print(totalStudents)
+    totalStudents = firstAndSecondYr + thirdAndFourthYr
+    print(firstAndSecondYr)
+    print(thirdAndFourthYr)
+    print(totalStudents)
